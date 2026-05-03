2d26a1ab1c0477b2ad1de419c5f22da1f33f1712
kajava-juri <jyri.kajava@tptlive.ee>
2026-05-03 22:01:14 +0300

    refactor ImuMessageHandler to log specific IMU data based on topic

--- server/backend/mqtt/handlers.go
+++ server/backend/mqtt/handlers.go
@@ -2,14 +2,25 @@ package handlers
 
 import (
 	"log"
-
+	"regexp"
 	mqtt "github.com/eclipse/paho.mqtt.golang"
 )
 
 // ImuMessageHandler returns a mqtt.MessageHandler that logs IMU topic payloads.
 func ImuMessageHandler() mqtt.MessageHandler {
 	return func(client mqtt.Client, msg mqtt.Message) {
-		log.Printf("IMU %s => %s", msg.Topic(), string(msg.Payload()))
+		r, _ := regexp.Compile("(accel|gyro)/(x|y|z)")
+		if r.MatchString(msg.Topic()) {
+			matches := r.FindStringSubmatch(msg.Topic())
+			accelType := matches[1] // acc or gyro
+			accelAxis := matches[2] // x, y, or z
+			switch accelType {
+				case "accel":
+					log.Printf("IMU linear acceleration %s => %s", accelAxis, string(msg.Payload()))
+				case "gyro":
+					log.Printf("IMU angular velocity %s => %s", accelAxis, string(msg.Payload()))
+			}
+		}
 	}
 }
 
